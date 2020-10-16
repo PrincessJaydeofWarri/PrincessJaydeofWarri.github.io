f6e465e7934d405e01816c97ade52a0733ea250e
Jayde Pearson <etana@Astute.local>
2020-10-16 10:34:30 +0100

    Updates

---
+++ precache-manifest.b3bfb14b0b591eed3f349eb947e270ea.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4ce468b9c2624e38f2deed166487e17",
+    "url": "/PrincessJaydeofWarri.github.io/index.html"
+  },
+  {
+    "revision": "b87345056e8b4dba02c1",
+    "url": "/PrincessJaydeofWarri.github.io/static/css/main.3e96f7f9.chunk.css"
+  },
+  {
+    "revision": "7d34b2a3bb96a7f24479",
+    "url": "/PrincessJaydeofWarri.github.io/static/js/2.36e9410b.chunk.js"
+  },
+  {
+    "revision": "ebc5d087fc4c4229c0680561aa363996",
+    "url": "/PrincessJaydeofWarri.github.io/static/js/2.36e9410b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b87345056e8b4dba02c1",
+    "url": "/PrincessJaydeofWarri.github.io/static/js/main.5910ab95.chunk.js"
+  },
+  {
+    "revision": "91350b8174a1727248b8",
+    "url": "/PrincessJaydeofWarri.github.io/static/js/runtime-main.a07cb244.js"
+  },
+  {
+    "revision": "eea87db5fe179489a469a9fb54b2f930",
+    "url": "/PrincessJaydeofWarri.github.io/static/media/Developherbg.eea87db5.mp4"
+  },
+  {
+    "revision": "d9914ac78e82fc5a724bf21a85d0da59",
+    "url": "/PrincessJaydeofWarri.github.io/static/media/MORTGAGE.d9914ac7.jpg"
+  },
+  {
+    "revision": "edb1e634ec4433e1e1622f58242991c6",
+    "url": "/PrincessJaydeofWarri.github.io/static/media/dloglow1.edb1e634.png"
+  },
+  {
+    "revision": "b353e98e8c18a5c076c535ba3babfa01",
+    "url": "/PrincessJaydeofWarri.github.io/static/media/jatcomp.b353e98e.jpg"
+  }
+]);
