a094d4d5219c057b2f12d592fa30e67e7849080a
Jayde Pearson <etana@Astute.local>
2020-11-15 15:47:14 +0000

    Updates

---
+++ precache-manifest.027621d19ae0817f142b474d519f54ce.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fe211442ed65335eda5bad362bdf856",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f5bc60f107fb673d5924",
+    "url": "/static/css/main.21947d24.chunk.css"
+  },
+  {
+    "revision": "d2ac22625aed63862619",
+    "url": "/static/js/2.c4e44dd8.chunk.js"
+  },
+  {
+    "revision": "ebc5d087fc4c4229c0680561aa363996",
+    "url": "/static/js/2.c4e44dd8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5bc60f107fb673d5924",
+    "url": "/static/js/main.253fadad.chunk.js"
+  },
+  {
+    "revision": "202fce87607a16d29b82",
+    "url": "/static/js/runtime-main.57e54692.js"
+  },
+  {
+    "revision": "eea87db5fe179489a469a9fb54b2f930",
+    "url": "/static/media/Developherbg.eea87db5.mp4"
+  },
+  {
+    "revision": "d9914ac78e82fc5a724bf21a85d0da59",
+    "url": "/static/media/MORTGAGE.d9914ac7.jpg"
+  },
+  {
+    "revision": "edb1e634ec4433e1e1622f58242991c6",
+    "url": "/static/media/dloglow1.edb1e634.png"
+  },
+  {
+    "revision": "b353e98e8c18a5c076c535ba3babfa01",
+    "url": "/static/media/jatcomp.b353e98e.jpg"
+  },
+  {
+    "revision": "7bfd6932633442e54ed20b05d4392da0",
+    "url": "/static/media/laptoppic0.7bfd6932.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/PrincessJaydeofWarri.github.io/precache-manifest.70980e97b5dfd7af3188cc2dd9d0b4ad.js"
+  "/precache-manifest.027621d19ae0817f142b474d519f54ce.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/PrincessJaydeofWarri.github.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
