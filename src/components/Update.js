91e86eec7afe96d0738ecb45e88f9fa9ffa86656
PrincessJaydeofWarri <wmweatherwoman@gmail.com>
2021-11-19 09:25:55 +0000

    logo updates

--- src/components/Update.js
+++ src/components/Update.js
@@ -1,18 +1,24 @@
 import React from "react";
 import "./Update.css";
 import Background from "./images/background.png";
+import logo from "./images/Developherj_logo.jpg";
 
 const Update = () => {
   return (
-    <div className="header-container">
-      <img src={Background} alt="jayde-logo" />
-      <h1 data-aos="fade-down" data-aos-delay="100">
-        Back soon
-      </h1>
-      <p data-aos="fade-up" data-aos-delay="100">
-        We are making some updates...
-      </p>
-    </div>
+    <>
+      <div className="logo">
+        <img src={logo} alt="logo" />
+      </div>
+      <div className="header-container">
+        <img src={Background} alt="jayde-logo" />
+        <h1 data-aos="fade-down" data-aos-delay="100">
+          Back soon
+        </h1>
+        <p data-aos="fade-up" data-aos-delay="100">
+          We are making some updates...
+        </p>
+      </div>
+    </>
   );
 };
 
